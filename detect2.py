fb5c82f2898e854dd24e3087535e3f68357e9052
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-05-12 19:42:48 +0200

    Fatal amanecer error corrected

--- detect2.py
+++ detect2.py
@@ -14,7 +14,7 @@ import math
 cap = cv2.VideoCapture(0)
 
 # model
-model = YOLO("runs/detect/Sausage1.6/weights/best.pt", verbose=False)
+model = YOLO("runs/detect/Everest1/weights/best.pt", verbose=False)
 #model.cuda()
 
 # object classes
