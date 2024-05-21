7063add83a48f26ee5a6ddd7ae25d69c91c6de52
Oriol <110051760+oriolgds@users.noreply.github.com>
2024-05-21 15:45:22 +0200

    Labeled and captured more GUERRA

--- detect2.py
+++ detect2.py
@@ -14,7 +14,7 @@ import math
 cap = cv2.VideoCapture(0)
 
 # model
-model = YOLO("runs/detect/Everest2.1/weights/best.pt", verbose=False)
+model = YOLO("runs/detect/Everest3/weights/best.pt", verbose=False)
 #model.cuda()
 
 # object classes
